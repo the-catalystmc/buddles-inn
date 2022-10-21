65ac979408de05b1065beed3dcb5b328a9e534a2
Rhyine <rhyinestewart@gmail.com>
2022-10-21 11:43:51 -0500

    Add update and delete functionalities

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -11,7 +11,7 @@ class RentalsController < ApplicationController
 
     def new
 		@rental = Rental.new
-	end
+	  end
 
     def create
         @rental = Rental.new(rental_params)
@@ -22,38 +22,47 @@ class RentalsController < ApplicationController
                 flash.now[:error] = "Rats! Fix your mistakes, please."
                 render :new, status: :unprocessable_entity
             end
-        end
+    end
   
     def destroy
-      @rental = rentals.find(params[:id])
+      @rentals = Rental.all
+      @rental = @rentals.find(params[:id])
       @rental.destroy
-      redirect_to room_path, notice: "Rental was successfully destroyed."
+      redirect_to rentals_path, notice: "Rental was successfully destroyed."
     end
 
     def edit
 		@rental = Rental.find(params[:id])
-	end
+	  end
 
     def update
+      @rental = Rental.find(params[:id])
         @room = Room.find(params[:id])
-        # if @rental.update(available: !@rental.available)
-        #   if @rental.available
-        #     flash[:notice] = 'Public'
-        #   else
-        #     flash[:error] = 'Not Public'
-        #   end
-        # else
-        #   flash[:error] = 'Public status couldn\'t be updated'
-        # end
-        # redirect_to rental_url(@rental)
-        @rental = Rental.find(params[:id])
-    
-        if @rental.update(rental_params)
-          redirect_to rental_path(@rental.id), notice: "Rental was successfully updated."
+        if @rental.update( 
+          occupant: @rental.occupant, 
+          check_in: !@rental.check_in,
+          check_out: !@rental.check_out,
+          shift: @rental.shift,
+          expected_checkout: @rental.expected_checkout,
+          start_date: @rental.start_date,
+          room_id: @rental.room_id
+        )
+          if @rental.check_out
+            flash[:notice] = 'Checked Out'
+          else
+            flash[:error] = 'Checked In'
+          end
         else
-          flash.now[:alert] = "Rats! Fix your mistakes, please."
-          render :edit, status: :unprocessable_entity
+          flash[:error] = 'Public status couldn\'t be updated'
         end
+        redirect_to rental_url(@rental)
+    
+        # if @rental.update(rental_params)
+        #   redirect_to rental_path(@rental.id), notice: "Rental was successfully updated."
+        # else
+        #   flash.now[:alert] = "Rats! Fix your mistakes, please."
+        #   render :edit, status: :unprocessable_entity
+        # end
       end
   
     private
